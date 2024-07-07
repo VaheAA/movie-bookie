8263978f48369d06816ad37208cf270d9fe0b19b
Abovyan Vahe <vahe.abov@gmail.com>
2024-07-07 15:43:43 +0400

    fix

--- api/config/db/index.ts
+++ api/config/db/index.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize'
 import config from '../db-vars'
 
 const sequelize = new Sequelize(
-	`postgres://${config.username}:${config.password}@${config.host}:5432/${config.database}`
+	`postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`
 )
 
 sequelize
